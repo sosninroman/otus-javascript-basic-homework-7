8908625f9d851fd90ec6b423820657e749895a55
r.sosnin <sosnin_roman@rambler.ru>
2025-04-01 23:18:57 +0500

    Add jest and write simple test

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,31 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginJest from 'eslint-plugin-jest';
 
 export default defineConfig([
   js.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ['**/*.js'],
     languageOptions: {
-      globals: globals.browser,
+      globals: { ...globals.browser },
     },
     rules: {
       'no-console': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: { ...pluginJest.environments.globals.globals },
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
     },
   },
   eslintPluginPrettierRecommended,
